e65cfdca7e0dadfbcc09c30bea6adfd17b07c916
hellodamii <daremiarchs@gmail.com>
2025-02-17 22:55:29 +0100

    animations

---
+++ Onboarding/View/CustomTextEffect.swift
@@ -0,0 +1,18 @@
+//
+//  CustomTextEffect.swift
+//  Onboarding
+//
+//  Created by Busha on 17/02/2025.
+//
+
+import SwiftUI
+
+struct CustomTextEffect: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomTextEffect()
+}
